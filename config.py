8ffe2f9f67772d18fcaf6c7d3205be7110cb7ad1
Oren Tapiero <orentapiero@Orens-MacBook-Pro.local>
2025-08-03 22:23:35 +0300

    Initial commit: Add crypto project files

---
+++ config.py
@@ -0,0 +1,35 @@
+schedule = {
+    'Sydney': {
+        'utc': '22:00 - 07:00',
+        'description': 'Asian Pacific session start'
+    },
+    'Tokyo': {
+        'utc': '00:00 - 09:00',
+        'description': 'Asian session'
+    },
+    'London': {
+        'utc': '08:00 - 17:00',
+        'description': 'European session'
+    },
+    'New_York': {
+        'utc': '13:00 - 22:00',
+        'description': 'North American session'
+    }
+}
+
+
+
+column_names = [
+    "open_time",
+    "open",
+    "high",
+    "low",
+    "close",
+    "volume",
+    "close_time",
+    "quote_asset_volume",
+    "number_of_trades",
+    "taker_buy_base_asset_volume",
+    "taker_buy_quote_asset_volume",
+    "ignore"
+]
